19aab10bdde9d25d6bf962540afabb9ee554a815
Santhosh <bgj.santhosh@gmail.com>
2020-02-06 07:08:10 +0530

    Updates

---
+++ precache-manifest.0752c56f060d068a0545d3f43f6ea794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852019759e18f813848b96a771024112",
+    "url": "/orderlinks/index.html"
+  },
+  {
+    "revision": "2abe3373379b5a37d2d7",
+    "url": "/orderlinks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a04d2ee7f5abe65eac17",
+    "url": "/orderlinks/static/js/2.28ba3b91.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/orderlinks/static/js/2.28ba3b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abe3373379b5a37d2d7",
+    "url": "/orderlinks/static/js/main.442e4a14.chunk.js"
+  },
+  {
+    "revision": "e0636c6fb20cf5ba3cfd",
+    "url": "/orderlinks/static/js/runtime-main.532593d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/orderlinks/static/media/logo.5d5d9eef.svg"
+  }
+]);
